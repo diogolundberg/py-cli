f10bddaa1bd809c3853996bf9a3eb2eb3eed02da
Diogo Lundberg <dclundberg@gmail.com>
2018-05-15 01:08:36 -0300

    Refactor s3 client

--- clients/s3.py
+++ clients/s3.py
@@ -2,23 +2,21 @@
 import boto3
 from os import getenv
 
+client = boto3.client(
+    's3',
+    aws_access_key_id=getenv('S3_ACCESS_KEY'),
+    aws_secret_access_key=getenv('S3_SECRET_KEY'),
+)
 
 def get(filename):
-    client = boto3.client(
-        's3',
-        aws_access_key_id=getenv('S3_ACCESS_KEY'),
-        aws_secret_access_key=getenv('S3_SECRET_KEY'),
-    )
-
     obj = client.get_object(Bucket='dataviva-etl', Key=filename)
     
     return obj['Body'].read()
 
 def put(filename, object):
-    client = boto3.client(
-        's3',
-        aws_access_key_id=getenv('S3_ACCESS_KEY'),
-        aws_secret_access_key=getenv('S3_SECRET_KEY'),
+    obj = client.put_object(
+        Bucket='api-metadata', 
+        Key=filename, 
+        Body=object, 
+        ContentType='application/json',
     )
-
-    obj = client.put_object(Bucket='api-metadata', Key=filename, Body=object, ContentType='application/json')
